a3c6893dd9dece4742c8e14189c95a253ba2fb98
Targo Dove <TargoDove@gmail.com>
2020-11-03 23:52:52 +0200

    Added two example files

---
+++ Example.cpp
@@ -0,0 +1,14 @@
+#include "Example.h"
+
+Example::Example()
+{}
+
+Example::~Example(){}
+
+void Example::method1(){
+  //Do nothing
+}
+
+void Example::method2(){
+  //Do nothing
+}
---
+++ Example.h
@@ -0,0 +1,17 @@
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+#include "Command.h"
+
+using namespace std;
+
+class Example
+{
+	public:
+    Example();
+    virtual ~Example();
+    virtual void method1();
+    virtual void method2();
+};
+
+#endif
