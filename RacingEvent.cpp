9a6b9ae38cb61c4e2059bbb3fe769279c80889e5
CaptainCo-dev <73959930+CaptainCo-dev@users.noreply.github.com>
2020-11-06 20:14:31 +0200

    Update RacingEvent.cpp

--- RacingEvent.cpp
+++ RacingEvent.cpp
@@ -1,6 +1,6 @@
 #include "RacingEvent.h"
 
-RacingEvent::RacingEvent(string name, Date sDate, RaceTrack trk) {
+RacingEvent::RacingEvent(string name, Date sDate, RaceTrack* trk) {
     raceName = name;
     startDate = sDate;
     track = trk;
