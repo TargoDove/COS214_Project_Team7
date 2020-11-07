58df718b46cdf0c9b7fbda6759a1d4f1882b42cc
CaptainCo-dev <73959930+CaptainCo-dev@users.noreply.github.com>
2020-11-07 16:26:43 +0200

    Update Friday.cpp

--- Friday.cpp
+++ Friday.cpp
@@ -1,6 +1,31 @@
 #include "Friday.h"
+#include <iostream>
 
-int Friday::simulateTrack(F1Car testCar) {
-	// TODO - implement Friday::simulateTrack
-	throw "Not yet implemented";
+Friday::Friday() {
+    bool cont=true;
+    int numRaces=5;
+    checkDay();
+    char in;
+    while (cont && numRaces!=0){
+        //run simlations until either tries are depleted or no longer want to race
+
+        cout<<"Do you want to do another practice lap? Y/N"<<endl;
+        cin>>in;
+        if (in){
+
+        }
+        else if (!in){
+            cont=false;
+        }
+        else{cout<<"Incorrect input, continue practice.";
+        }
+    }
+}
+
+void Friday::checkDay() {
+	cout<<"Today is Friday, Practice Day."<<endl;
+}
+
+void Friday::handleNextDay(RacingDay* day) {
+    day->setRacingDay( new Saturday());
 }
