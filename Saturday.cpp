633fe0c9cc27ec0228df140edb05c1e738a0add0
CaptainCo-dev <73959930+CaptainCo-dev@users.noreply.github.com>
2020-11-07 16:27:33 +0200

    Update Saturday.cpp

--- Saturday.cpp
+++ Saturday.cpp
@@ -1,11 +1,31 @@
 #include "Saturday.h"
+#include <iostream>
 
-void Saturday::addQualifyScore() {
-	// TODO - implement Saturday::addQualifyScore
-	throw "Not yet implemented";
+Saturday::Saturday() {
+    bool cont=true;
+    int numRaces=5;
+    checkDay();
+    while (cont && numRaces!=0){
+        //run simlations until either tries are depleted or no longer want to race
+
+        cout<<"Do you want to do another practice lap? Y/N"<<endl;
+        cin>>in;
+        if (in){
+
+        }
+        else if (!in){
+            cont=false;
+        }
+        else{cout<<"Incorrect input, continue practice.";
+        }
+    }
+    //call qualify race
 }
 
-void Saturday::setGridLayout(F1Team teamList) {
-	// TODO - implement Saturday::setGridLayout
-	throw "Not yet implemented";
+void Saturday::checkDay() {
+	cout<<"Today is Saturday, practice then qualifying."<<endl;
+}
+
+void Saturday::handleNextDay(RacingContext* day) {
+	day->setRacingDay( new Sunday());
 }
