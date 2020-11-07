d79f7ef1b86d8d13a23cdce0846faaa438b9fd33
CaptainCo-dev <73959930+CaptainCo-dev@users.noreply.github.com>
2020-11-07 21:33:43 +0200

    Update Racing.cpp

--- Racing.cpp
+++ Racing.cpp
@@ -24,14 +24,15 @@ void Racing::swap(F1Team *xteam, F1Team *yteam)
 }
 
 void Racing::addFinalScore(F1Team** team) {
+	//sort teams into finish order
     int n = sizeof(team);
     int i, j;
     for (i = 0; i < n-1; i++)
-        // Last i elements are already in place
         for (j = 0; j < n-i-1; j++)
-            if (team[j]->getF1Car(0)->getRaceScore > team[j+1]->getF1Car(0)->getRaceScore)
+            if (team[j]->getF1Car(0)->getRaceScore < team[j+1]->getF1Car(0)->getRaceScore)
                 swap(&team[j], &team[j+1]);
     }
+//give top 10 their points for the race
     team[0]->points+=25;
     team[1]->points+=18;
     team[2]->points+=15;
@@ -43,44 +44,31 @@ void Racing::addFinalScore(F1Team** team) {
     team[8]->points+=2;
     team[9]->points+=1;
 	//need to make sure
+	for (int x = 0; x <n; x++) {
+        	finishOrder[x] = team[x];
+    }
+
 }
 
-void Racing::addQualifyScore(F1Team* team) {
-	sort;
+F1Team** Racing::addQualifyScore(F1Team** team) {
+	//race one lap and will each car a time
+	//this time will then sort them into an array of teams from fastest to slowest
+	//similar to addFinalScore
+	//returns an array
 }
 
-void Racing::raceLapOrder() {
-	// TODO - implement Racing::simulateLapOrder
-	throw "Not yet implemented";
-}
-
-void Racing::raceLapScore() {
-	// TODO - implement Racing::simulateLapTime
-	throw "Not yet implemented";
+double Racing::raceLapScore() {
+	//will inherit from lapElement to race a lap and generate a time
 }
 
 string Racing::performPitstop(boolean pitstop) {
-	// TODO - implement Racing::performPitstop
-	throw "Not yet implemented";
+	//used to call pitstop class
 }
 
-void Racing::raceLap() {
-	// TODO - implement Racing::raceLap
-	throw "Not yet implemented";
+F1Team** Racing::raceEvent(F1Teams** teams) {
+	//called after constructor, it encapsualtes multiple functions
+	// will take all teams and use raceLapScore for the amount of laps, adding each time to a total time
+	//will then use addFinalScore to sort teams
+	//returns the sorted list to be used by prize giving
 }
 
-int Racing::tallyPoints() {
-	// TODO - implement Racing::tallyPoints
-	throw "Not yet implemented";
-}
-
-void Racing::raceEvent(int numLaps) {
-	// TODO - implement Racing::raceEvent
-	throw "Not yet implemented";
-}
-
-
-void Racing::raceEvent() {
-	// TODO - implement Racing::raceEvent
-	throw "Not yet implemented";
-}
