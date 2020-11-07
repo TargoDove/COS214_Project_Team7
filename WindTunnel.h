12d35e32b0da38ba8727cffdf3aecb8937096d58
MoeMoola3 <73942516+MoeMoola3@users.noreply.github.com>
2020-11-07 19:52:22 +0200

    Update WindTunnel.h

--- WindTunnel.h
+++ WindTunnel.h
@@ -1,10 +1,14 @@
-class WindTunnel {
+#include "TestingMethod.h"
+
+class WindTunnel:TestingMethod {
 private:
     int Tokens;
 public:
     WindTunnel();
+    WindTunnel(int);
     virtual ~WindTunnel();
     int getTokens();
     void decrement();
     double displayPerformance();
+    virtual void testPerformance(double);
 };
